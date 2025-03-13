25a5f7c1fa2b232b65d7f1f022ad0b2dc5d640e2
Fariha Tabassum <fariha.t13@gmail.com>
2025-03-12 21:38:43 -0500

    feat: Create setup-cloud.sh

---
+++ setup-cloud.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget https://farihat13.github.io/s/bashrc -O .bashrc
+wget https://farihat13.github.io/s/vimrc -O .vimrc
+wget https://farihat13.github.io/s/tmux.conf -O .tmux.conf
+
+mkdir ~/scripts
+cd ~/scripts
+wget https://farihat13.github.io/s/system-info.sh
+wget https://farihat13.github.io/s/mount-disk.sh
+wget https://farihat13.github.io/s/unmount-disk.sh
+chmod u+x ./*
+
+sudo apt update
+sudo apt install xclip
